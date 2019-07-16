ebdf7c09a6e3e6985b36ed97872eb524b1397aa1
Sokly <hengsokly23@gmail.com>
2019-07-16 10:20:49 +0700

    enhance master layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,5 +18,6 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+//= require sidebar
 
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "events#index"
+  root to: "home#index"
   devise_for :users, path: '/'
+
+  resources :users
+  resources :events
 end
