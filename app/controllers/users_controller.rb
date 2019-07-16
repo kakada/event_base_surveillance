43389982e019d888033342e1c2411667b1732602
Sokly <hengsokly23@gmail.com>
2019-07-16 15:36:43 +0700

    add delete user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_url
+  end
+
   private
 
   def user_params
