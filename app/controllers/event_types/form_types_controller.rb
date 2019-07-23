40a326ca28e456256af10100e586bfca4201b3bd
Sokly <hengsokly23@gmail.com>
2019-07-23 08:50:22 +0700

    fix bug

--- app/controllers/event_types/form_types_controller.rb
+++ app/controllers/event_types/form_types_controller.rb
@@ -11,9 +11,9 @@ module EventTypes
     end
 
     def create
-      @form_type = @event_type.form_types.new(form_params)
+      @form_type = @event_type.form_types.new(form_type_params)
       if @form_type.save
-        redirect_to event_type_forms_url
+        redirect_to event_type_form_types_url
       else
         render :new
       end
@@ -26,7 +26,7 @@ module EventTypes
     def update
       @form_type = FormType.find(params[:id])
 
-      if @form_type.update_attributes(form_params)
+      if @form_type.update_attributes(form_type_params)
         redirect_to event_type_forms_url
       else
         render :edit
@@ -37,13 +37,13 @@ module EventTypes
       @form_type = FormType.find(params[:id])
       @form_type.destroy
 
-      redirect_to event_type_forms_url
+      redirect_to event_type_form_types_url
     end
 
     private
 
-    def form_params
-      params.require(:form).permit(:name, fields_attributes: [:id, :name, :filed_type, :required, :_destroy])
+    def form_type_params
+      params.require(:form_type).permit(:name, fields_attributes: [:id, :name, :field_type, :required, :_destroy])
     end
 
     def assign_event_type
