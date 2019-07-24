a2e968bea9368e73ddf9b365c6fbf92db1b9256c
Sokly <hengsokly23@gmail.com>
2019-07-24 11:56:36 +0700

    refactor to use different master layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   before_action :authenticate_user!
+
+  layout :set_layout
+
+  protected
+
+  def set_layout
+    devise_controller? ? 'layouts/minimal' : 'layouts/application'
+  end
 end
