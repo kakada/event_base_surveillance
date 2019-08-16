baef82cb7af922d084eff88eddbd0014caa9e15f
Borey <lim.borey@gmail.com>
2019-08-16 15:16:28 +0700

    map virtualization: add program filter

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,13 +2,14 @@ class MapsController < ApplicationController
   before_action :set_date_range
   def index
     events = Event.where(options).group(:latitude, :longitude, :event_type_id).count
-    @event_data = build_event_data(events)
-    @event_types = EventType.all
+    @event_data = get_event_data(events)
+    @event_types = EventType.select(:id, :name).where(program_option)
     @event_type_id = params[:event_type]
+    @programs = Program.select(:id, :name).order(name: :asc) if current_user.system_admin?
   end
 
   private
-  def build_event_data(events)
+  def get_event_data(events)
     event_data = []
     events.each do |data, value|
       event_type = EventType.find(data[2])
@@ -31,6 +32,7 @@ class MapsController < ApplicationController
     option = {}
     option.merge!({ event_type_id: params[:event_type]}) if params[:event_type].present?
     option.merge!({ event_date: @start_date..@end_date })
+    option.merge! program_option
   end
 
   def set_date_range
@@ -38,8 +40,13 @@ class MapsController < ApplicationController
       @start_date = Date.strptime(params[:daterange].split(' - ')[0], "%m/%d/%Y")
       @end_date = Date.strptime(params[:daterange].split(' - ')[1], "%m/%d/%Y")
     else
-      @start_date = Time.zone.today - 30
+      @start_date = Time.zone.today - 29
       @end_date = Time.zone.today
     end
+  end
+
+  def program_option
+    program_id = current_user.program_id || params[:program_id]
+    if program_id.present? then { program_id: program_id } else {} end
   end
 end
