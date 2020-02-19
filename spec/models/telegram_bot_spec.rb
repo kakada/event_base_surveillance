0f192a6afe6ac7e6df22c243c55e448b3b12de00
Sokly <hengsokly23@gmail.com>
2020-02-19 14:42:05 +0700

    add spec

--- spec/models/telegram_bot_spec.rb
+++ spec/models/telegram_bot_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe TelegramBot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:program) }
+
+  context 'is enabled bot' do
+    before { allow(subject).to receive(:enabled?).and_return(true) }
+
+    it { is_expected.to validate_presence_of(:token) }
+    it { is_expected.to validate_presence_of(:username) }
+  end
+
+  context 'is not enabled bot' do
+    before { allow(subject).to receive(:enabled?).and_return(false) }
+
+    it { is_expected.not_to validate_presence_of(:token) }
+    it { is_expected.not_to validate_presence_of(:username) }
+  end
 end
