01721e9833cd7657455cc3f5a5fbc9ce8bc7b2b9
Sokly <hengsokly23@gmail.com>
2020-02-20 14:23:08 +0700

    rename environment variable

--- app/models/telegram_bot.rb
+++ app/models/telegram_bot.rb
@@ -7,7 +7,7 @@ class TelegramBot < ApplicationRecord
   before_update :post_webhook_to_telegram
 
   def post_webhook_to_telegram
-    request = RestClient::Request.new( method: :post, url: "https://api.telegram.org/bot#{token}/setWebhook?url=#{ENV['DOMAIN_HTTPS']}/telegram")
+    request = RestClient::Request.new( method: :post, url: "https://api.telegram.org/bot#{token}/setWebhook?url=#{ENV['TELEGRAM_CALLBACK_URL']}")
     response = request.execute {|response| response}
     self.actived = (response.code == 200)
   end
