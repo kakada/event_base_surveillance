c3c4886ec4bf411a8bbf030069ec1c727ffddd5c
Sokly <hengsokly23@gmail.com>
2020-02-27 11:38:07 +0700

    add spec to map service

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,7 +4,7 @@ class MapsController < ApplicationController
   before_action :set_date_range
 
   def index
-    @event_types = current_program.event_types
+    @event_types = EventType.with_shared(current_program.id)
     @event_data = MapService.new(current_user).get_event_data(params)
   end
 
--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -3,12 +3,11 @@
 class MapService
   def initialize(user)
     @user = user
-    @program = user.program
-    @event_types = @program.event_types
+    @event_types = EventType.with_shared(user.program_id)
   end
 
-  def get_event_data(params)
-    data_by_field_code = query_group_by_field_code
+  def get_event_data(params={})
+    all_cases = query_group_by_field_code
     events_hash = Pundit.policy_scope(@user, Event.filter(params).joins(:event_type, :location).group('locations.latitude', 'locations.longitude', 'events.event_type_id', 'locations.name_km', 'events.location_code')).count
 
     events_hash.map do |data, value|
@@ -22,9 +21,9 @@ class MapService
         lat: data[0],
         lng: data[1],
         location: data[3],
-        number_of_case: data_by_field_code.select{ |obj| obj['event_type_id'] == event_type.id && obj['location_code'] == data[4] && obj['field_code'] == 'number_of_case' }.first.try(:[], 'total'),
-        number_of_death: data_by_field_code.select{ |obj| obj['event_type_id'] == event_type.id && obj['location_code'] == data[4] && obj['field_code'] == 'number_of_death' }.first.try(:[], 'total'),
-        number_of_hospitalized: data_by_field_code.select{ |obj| obj['event_type_id'] == event_type.id && obj['location_code'] == data[4] && obj['field_code'] == 'number_of_hospitalized' }.first.try(:[], 'total')
+        number_of_case: all_cases.select{ |obj| obj['event_type_id'] == event_type.id && obj['location_code'] == data[4] && obj['field_code'] == 'number_of_case' }.first.try(:[], 'total'),
+        number_of_death: all_cases.select{ |obj| obj['event_type_id'] == event_type.id && obj['location_code'] == data[4] && obj['field_code'] == 'number_of_death' }.first.try(:[], 'total'),
+        number_of_hospitalized: all_cases.select{ |obj| obj['event_type_id'] == event_type.id && obj['location_code'] == data[4] && obj['field_code'] == 'number_of_hospitalized' }.first.try(:[], 'total')
       }
     end
   end
@@ -32,17 +31,13 @@ class MapService
   private
     # https://stackoverflow.com/questions/20926615/postgresql-aggregate-sum-with-condition
     def query_group_by_field_code
-      sql= "SELECT event_type_id, location_code, field_code,
-        SUM(value::decimal) AS total
-      FROM (
-        SELECT fv.*, ev.*
-        FROM   field_values fv, events ev
-        WHERE  ev.uuid = fv.valueable_id
-               AND fv.valueable_type='Event'
-               AND ev.program_id = #{@program.id}
-               AND (fv.field_code='number_of_case' OR field_code='number_of_hospitalized' OR field_code='number_of_death')
-      ) as Alias
-      GROUP  BY event_type_id, location_code, field_code";
+      sql = "SELECT event_type_id, location_code, field_code, SUM(value::decimal) AS total
+        FROM field_values fv
+             INNER JOIN events ev ON (ev.uuid = valueable_id AND fv.valueable_type='Event')
+             INNER JOIN event_types et ON (ev.event_type_id = et.id)
+        WHERE fv.field_code in ('number_of_case', 'number_of_hospitalized', 'number_of_death')
+              AND (ev.program_id=#{@user.program_id} OR et.shared=true)
+        GROUP BY event_type_id, location_code, field_code";
 
       ActiveRecord::Base.connection.execute(sql).to_a
     end
