37113618fc48ea91631f61fac5a18ea5fddeb18b
Sokly <hengsokly23@gmail.com>
2020-03-13 09:32:59 +0700

    translation and add icon

--- app/models/event.rb
+++ app/models/event.rb
@@ -72,7 +72,7 @@ class Event < ApplicationRecord
     scope = all
     scope = scope.joins(:event_type).where('LOWER(event_types.name) LIKE ?', "%#{keywords[1].downcase}%") if keywords.present? && keywords[0] == 'suspected_event'
     scope = scope.joins(:field_values).where('field_values.field_code = ? and field_values.value = ?', keywords[0], keywords[1]) if keywords.present? && keywords[0] != 'suspected_event'
-    scope = scope.joins(:field_values).where('field_values.field_code = ? and field_values.value >= ?', 'event_date', params[:start_date]) if params[:start_date].present?
+    scope = scope.where('event_date >= ?', params[:start_date]) if params[:start_date].present?
     scope = scope.where(event_type_id: params[:event_type_id]) if params[:event_type_id].present?
     scope = scope.joins(:event_type).where('events.uuid LIKE ? OR LOWER(event_types.name) LIKE ?', "%#{params[:search]}%", "%#{params[:search].downcase}%") if params[:search].present?
     scope
