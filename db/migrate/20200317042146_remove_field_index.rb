eafa0d55700ab27c4433b11e37778338f9dd2430
Sokly <hengsokly23@gmail.com>
2020-03-17 11:41:29 +0700

    Fix duplicate field name: remove feild index

---
+++ db/migrate/20200317042146_remove_field_index.rb
@@ -0,0 +1,6 @@
+class RemoveFieldIndex < ActiveRecord::Migration[5.2]
+  def change
+    remove_index :fields, name: 'index_fields_on_milestone_id_and_code' if index_exists?(:fields, name: 'index_fields_on_milestone_id_and_code')
+    remove_index :fields, name: 'index_fields_on_milestone_id_and_name' if index_exists?(:fields, name: 'index_fields_on_milestone_id_and_name')
+  end
+end
