3554d18ccce92782dbd76fd481cb818cbacd5914
Sokly <hengsokly23@gmail.com>
2020-07-16 14:09:00 +0700

    enhance icon and sidebar

--- app/helpers/milestones_helper.rb
+++ app/helpers/milestones_helper.rb
@@ -5,21 +5,21 @@ module MilestonesHelper
     return '' unless milestone.message.present?
 
     if milestone.program.telegram_bot.nil? || !milestone.program.telegram_bot.actived?
-      return '<a class="btn btn-small text-danger" title="Telegram bot is invalid configure" data-toggle="tooltip"><i class="fab fa-telegram"></i></a>'
+      return '<a class="btn btn-small text-secondary position-relative" title="Telegram bot is invalid configure" data-toggle="tooltip"><i class="fab fa-telegram font-size-24"></i><i class="far fa-times-circle icon-red invalid-config-icon"></i></a>'
     elsif !milestone.program.telegram_bot.enabled? || milestone.telegram_notification.nil? || milestone.telegram_notification.chat_groups.blank?
-      return '<a class="btn btn-small text-secondary" title="Telegram bot is disabled or no chat groups selected" data-toggle="tooltip"><i class="fab fa-telegram"></i></a>'
-    else
-      return "<a class='btn btn-small text-success' data-trigger='hover' data-content='#{milestone.telegram_notification.chat_groups.pluck(:title).join('; ')}' data-toggle='popover' title='Telegram Chat Group'><i class='fab fa-telegram'></i></a>"
+      return '<a class="btn btn-small text-secondary" title="Telegram bot is disabled or no group notification selected" data-toggle="tooltip"><i class="fab fa-telegram font-size-24"></i></a>'
     end
+
+    "<a class='btn btn-small text-success' data-trigger='hover' data-content='#{milestone.telegram_notification.chat_groups.pluck(:title).join('; ')}' data-toggle='popover' title='Notify to'><i class='fab fa-telegram font-size-24 active-telegram-icon'></i></a>"
   end
 
   def email_icon_status(milestone)
     return '' unless milestone.message.present?
 
     if !milestone.program.enable_email_notification? || milestone.email_notification.nil? || milestone.email_notification.emails.blank?
-      return '<a class="btn btn-small text-secondary" title="Email is disabled or no emails selected" data-toggle="tooltip"><i class="far fa-envelope"></i></a>'
-    else
-      return "<a class='btn btn-small text-success' data-trigger='hover' data-content='#{milestone.email_notification.emails.join('; ')}' data-toggle='popover' title='Email group'><i class='fas fa-envelope'></i></a>"
+      return '<a class="btn btn-small text-secondary" title="Email is disabled or no email notification selected" data-toggle="tooltip"><i class="far fa-envelope font-size-24"></i></a>'
     end
+
+    "<a class='btn btn-small text-danger' data-trigger='hover' data-content='#{milestone.email_notification.emails.join('; ')}' data-toggle='popover' title='Notify to'><i class='far fa-envelope font-size-24'></i></a>"
   end
 end
