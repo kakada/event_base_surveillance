15194985dd132437d86b1b66ca556d5cd4c69fe8
Sokly <hengsokly23@gmail.com>
2020-08-13 16:04:17 +0700

    tracing service: refactor long method

--- app/services/tracing_service.rb
+++ app/services/tracing_service.rb
@@ -9,22 +9,37 @@ class TracingService
 
   def text_tracings
     field_ids = @event.program.fields.tracking.pluck(:id)
-    tracings = Tracing.text.where(field_id: field_ids).where("(traceable_id = ? AND traceable_type = 'Event') OR (traceable_id IN (?) AND traceable_type = 'EventMilestone')", @event.id, @event.event_milestone_ids.join(','))
-    tracings = tracings.as_json.each do |tracing|
-      tracing['created_date'] = I18n.l(tracing['created_at'], format: :y_m_d_h_mn)
-    end
+    tracings = get_text_tracings_by_fields(field_ids)
+    tracings = add_field_created_date(tracings)
 
-    hash_obj = {}
-
-    field_ids.each do |field_id|
-      sub_tracings = tracings.select { |tracing| tracing['field_id'] == field_id }
-
-      hash_obj[field_id] = {
-        display_able: sub_tracings.length > 1,
-        tracings: sub_tracings.last(5)
-      }
-    end
-
-    hash_obj
+    build_field_tracings_hash(field_ids, tracings)
   end
+
+  private
+    def get_text_tracings_by_fields(field_ids)
+      Tracing.text.where(field_id: field_ids).where("(traceable_id = ? AND traceable_type = 'Event') OR (traceable_id IN (?) AND traceable_type = 'EventMilestone')", @event.id, @event.event_milestone_ids.join(','))
+    end
+
+    def add_field_created_date(tracings)
+      tracings.as_json.each do |tracing|
+        tracing['created_date'] = I18n.l(tracing['created_at'], format: :y_m_d_h_mn)
+      end
+
+      tracings
+    end
+
+    def build_field_tracings_hash(field_ids, tracings)
+      hash_obj = {}
+
+      field_ids.each do |field_id|
+        sub_tracings = tracings.select { |tracing| tracing['field_id'] == field_id }
+
+        hash_obj[field_id] = {
+          display_able: sub_tracings.length > 1,
+          tracings: sub_tracings.last(5)
+        }
+      end
+
+      hash_obj
+    end
 end
