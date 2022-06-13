7844dca18b16f4441c57870057e55b9ccab76b45
Sokly <hengsokly23@gmail.com>
2022-06-13 14:55:20 +0700

    Migration: fix change schedule emails column type

--- db/migrate/20220531041744_add_deadline_duration_in_day_to_schedules.rb
+++ db/migrate/20220531041744_add_deadline_duration_in_day_to_schedules.rb
@@ -3,6 +3,6 @@
 class AddDeadlineDurationInDayToSchedules < ActiveRecord::Migration[5.2]
   def change
     add_column :schedules, :deadline_duration_in_day, :integer
-    change_column :schedules, :emails, :text, array: true, default: []
+    change_column :schedules, :emails, :text, array: true, default: [], using: "emails::text[]"
   end
 end
