8865d1db346b4a4e9745f0e4bc0357ab9c49064e
Sokly <hengsokly23@gmail.com>
2022-08-24 16:26:22 +0700

    Sign in: add recaptcha, enhance header pdf view

--- Gemfile
+++ Gemfile
@@ -92,6 +92,8 @@ gem "pdf-reader", "~> 2.5.0"
 
 gem "ipaddress", "~> 0.8.3"
 
+gem "recaptcha", "~> 5.10.0"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
 
 class SessionsController < Devise::SessionsController
+  include UsersHelper
+  prepend_before_action :verify_captcha, only: [:create] # Change this to be any actions you want to protect.
   prepend_after_action :set_sign_in_type, only: [:create]
 
   private
     def set_sign_in_type
       resource.update_column(:sign_in_type, User::SYSTEM)
+    end
+
+    def verify_captcha
+      return if disable_recaptcha? || verify_recaptcha # verify_recaptcha(action: 'login') for v3
+
+      self.resource = resource_class.new sign_in_params
+
+      respond_with_navigational(resource) do
+        flash.discard(:recaptcha_error) # We need to discard flash to avoid showing it on the next page reload
+        render :new
+      end
     end
 end
